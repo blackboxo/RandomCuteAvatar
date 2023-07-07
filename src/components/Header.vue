fbae5fc1a7dd6568d56f383839ae7b7013228726
blackboxo <592276898@qq.com>
2023-07-07 12:58:22 +0800

    change product name

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="absolute left-4 top-4 md:left-8 md:top-8 flex items-center cursor-pointer" @click="toHome">
         <img class="w-14 rounded-full hover:shadow-lg " src="@/assets/logo.gif" alt="logo" />
-        <h2 class="text-left pl-3">Random <br> Cute Avatar</h2>
+        <h2 class="text-left text-lg pl-3">Avatar Box</h2>
     </div>
     <!-- switch zh/en language-->
     <div class="absolute rounded-full right-4 top-6 md:right-8 md:top-10 hover:shadow-lg">
